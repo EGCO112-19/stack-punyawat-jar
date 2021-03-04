c486f13c718b6d6f4501c6ac1e42bd857049daaf
Punyawat Jaroensiripong <punyawat0@gmail.com>
2021-03-04 14:26:08 +0000

    fix stack.h

--- stack.h
+++ stack.h
@@ -37,8 +37,8 @@ void pop_all(StackPtr s){
         value = t->data;
         printf("Popping : '%c'\n",value);
         s->top = t->nextPtr;
-        free(t);
         t = s->top;
+        free(t);
     }
     s->size=0;
         
